2a29839e6797cb61e8b1f47be30b4e7e21600af2
Willson Li <willson@Willsons-MacBook-Pro.local>
2019-05-07 21:05:33 -0400

    Updates

---
+++ precache-manifest.f25d1596158e0a7fccb4a10b8331a410.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "a11ef42f18176907fd7b",
+    "url": "/libraryReact/static/js/runtime~main.8623dc1d.js"
+  },
+  {
+    "revision": "0da033b358beeadb57f1",
+    "url": "/libraryReact/static/js/main.d614bdb2.chunk.js"
+  },
+  {
+    "revision": "9eead0b73cd2b43317bb",
+    "url": "/libraryReact/static/js/2.acb9a3e8.chunk.js"
+  },
+  {
+    "revision": "bff54fdfb81bbd4a9786fe17d65e7676",
+    "url": "/libraryReact/index.html"
+  }
+];
